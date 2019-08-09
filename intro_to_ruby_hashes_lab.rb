120cf482fc3c6fc6a0d2fd01278e70aec6354774
Jeffrey Brown <brownjs03@gmail.com>
2019-08-09 14:13:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-my_hash_creator(:key, value)
+{key => value}
 end
 
 def read_from_hash(hash, key)
@@ -23,5 +23,6 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-update_counting_hash(:hash, key) = +1 
+hash[key] ? hash[key] += 1 : hash[key] = 1 
+hash
 end
